a742851ad7dd8fa9c35846db98a67f994fe589ea
BennyThink <benny.think@gmail.com>
2020-10-29 13:00:33 +0800

    upper case

--- teletweet/config.py
+++ teletweet/config.py
@@ -9,12 +9,12 @@ __author__ = "Benny <benny.think@gmail.com>"
 
 import os
 
-bot_token = os.environ.get("token") or ""
-consumer_key = os.environ.get("consumer_key") or ""
-consumer_secret = os.environ.get("consumer_secret") or ""
-access_token_key = os.environ.get("access_key") or ""
-access_token_secret = os.environ.get("access_secret") or ""
-proxy_setup = os.environ.get("proxy")
-owner = os.environ.get("owner") or ""
+bot_token = os.environ.get("TOKEN") or ""
+consumer_key = os.environ.get("CONSUMER_KEY") or ""
+consumer_secret = os.environ.get("CONSUMER_SECRET") or ""
+access_token_key = os.environ.get("ACCESS_KEY") or ""
+access_token_secret = os.environ.get("ACCESS_SECRET") or ""
+proxy_setup = os.environ.get("PROXY")
+owner = os.environ.get("OWNER") or ""
 
 tweet_format = "https://twitter.com/{screen_name}/status/{id}"
