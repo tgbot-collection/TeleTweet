df482638755de3ecac6e77bf8d08ebf03a550910
BennyThink <benny.think@gmail.com>
2020-10-31 22:14:30 +0800

    multi-user auth token

---
+++ teletweet/helper.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+
+# TeleTweet - helper.py
+# 10/31/20 21:57
+#
+
+__author__ = "Benny <benny.think@gmail.com>"
+
+import json
+
+
+def can_use(chat_id) -> bool:
+    with open("database.json") as f:
+        data: dict = json.load(f)
+    return data.get(chat_id)
